2240e8edd049969c9570a4b9efc88ae35d1507d4
fenek644 <fenek644@ya.ru>
2019-03-25 02:45:29 +0300

    Add config for Factory Bot

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,7 @@ require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+# require 'support/factory_girl'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -24,6 +25,10 @@ require 'rspec/rails'
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
+#
+RSpec.configure do |config|
+  config.include FactoryBot::Syntax::Methods
+end
 
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
