478ad295fba508ce28da9fc7a81db7f40364f04e
fenek644 <fenek644@ya.ru>
2019-03-28 18:28:05 +0300

    update create contact test

--- spec/features/visitor_create_contact_spec.rb
+++ spec/features/visitor_create_contact_spec.rb
@@ -4,6 +4,6 @@ feature "Contact creation" do
   scenario "allows access to contact page"do
     visit '/contacts'
 
-    expect(page).to have_content 'Email'
+    expect(page).to have_content I18n.t('contacts.contact_us')
   end
 end
