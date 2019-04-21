1026cb45ecb913a431b30e00ea809f6fe6803249
fenek644 <fenek644@ya.ru>
2019-04-21 19:22:41 +0300

    Add validation of the body of comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :article
+
+  validates :body, length:  {maximum: 4000}
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,5 +2,12 @@
 require "rails_helper"
 
 describe  Comment do
-  it { should belong_to :article }
+  describe "association" do
+    it { should belong_to :article }
+  end
+
+  describe "validate_length_of_comment" do
+    it { should validate_length_of(:body).is_at_most(4000)}
+  end
+
 end
