3742349f6486130ccbb1f34eca4ed9c050eebef6
fenek644 <fenek644@ya.ru>
2019-04-22 21:31:57 +0300

    Update create contact test, using i18n.

--- spec/features/visitor_create_contact_spec.rb
+++ spec/features/visitor_create_contact_spec.rb
@@ -3,6 +3,6 @@ require "rails_helper"
 feature "Contact Creation" do
   scenario "allow acces to contactы page" do
     visit '/contacts'
-    expect(page).to have_content 'New Contact'
+    expect(page).to have_content I18n.t 'contacts.contact_us'
   end
 end
