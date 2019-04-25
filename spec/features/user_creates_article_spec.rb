f91a4691c10fe8b3e77f21ac56feff8f039d0698
fenek644 <fenek644@ya.ru>
2019-04-25 19:47:52 +0300

    Update user_create_article_spec

---
+++ spec/features/user_creates_article_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+feature "Article Creation" do
+
+  before(:all) do
+    sign_up
+  end
+
+  scenario "allow user to visit new article page" do
+
+    visit new_article_path
+
+    expect(page).to have_content 'New Article'
+
+  end
+
+end
