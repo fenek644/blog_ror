a9161fd61030c6f821b1850d888b0003af13efb5
fenek644 <fenek644@ya.ru>
2019-04-25 19:50:28 +0300

    update visitor_create_account_spec

--- spec/features/visitor_creates_account_spec.rb
+++ spec/features/visitor_creates_account_spec.rb
@@ -1,8 +1,12 @@
 require "rails_helper"
+require 'database_cleaner'
 
 feature "Account Creation" do
-  scenario "allow guest to create account" do
+  before(:all) do
     sign_up
+  end
+  scenario "allow guest to create account" do
+    # sign_up
 
     expect(page).to have_content I18n.t('devise.registrations.signed_up')
 
@@ -10,15 +14,3 @@ feature "Account Creation" do
 
 end
 
-# def sign_up
-#
-#   visit new_user_registration_path
-#
-#   fill_in :user_username, with: 'user'
-#   fill_in :user_email, with: 'user@example.com'
-#   fill_in :user_password, with: '123456'
-#   fill_in :user_password_confirmation, with: '123456'
-#
-#   click_button 'Sign up'
-#
-# end
