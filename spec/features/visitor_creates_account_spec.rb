dbe35f36a3b95e1ee31eb3795e8bf358fe6f874e
fenek644 <fenek644@ya.ru>
2019-04-29 15:55:44 +0300

    fix bug with working of database_cleaner
    the main point - to rename user_creates_article_spec.rb in visitor_creates-.....

--- spec/features/visitor_creates_account_spec.rb
+++ spec/features/visitor_creates_account_spec.rb
@@ -1,12 +1,10 @@
 require "rails_helper"
-require 'database_cleaner'
+
 
 feature "Account Creation" do
-  before(:all) do
-    sign_up
-  end
+
   scenario "allow guest to create account" do
-    # sign_up
+    sign_up
 
     expect(page).to have_content I18n.t('devise.registrations.signed_up')
 
